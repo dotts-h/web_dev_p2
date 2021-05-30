6b5d9df1442b13d8ac5b85ce70f9d7d597a1d97d
Horia C. Nuță <n.horia.c@hotmail.com>
2021-05-30 18:57:26 +0300

    added p2 link

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <section id="proiecte">
         <p>Proiectele mele pentru Disciplina Tehnologii Web</p>
         <h2><a href="https://dotts-h.github.io/web_dev_p1/index.html">Proiect - 1</a></h2>
-        <h2>Proiect - 2</h2>
+        <h2><a href="https://hidden-anchorage-21066.herokuapp.com/index.php">Proiect - 2</h2>
         <h2>Proiect - 3</h2>
     </section>
 
